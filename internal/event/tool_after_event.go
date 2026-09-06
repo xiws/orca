5cc6c12f347d9782f1ffb66c11a2fd9b9239d449
xiw <zhongxiwang@foxmail.com>
2026-09-06 17:34:47 +0800

    feat(agent): work space

--- internal/event/tool_after_event.go
+++ internal/event/tool_after_event.go
@@ -3,6 +3,7 @@ package event
 import (
 	"fmt"
 	"orca/pkg/event"
+	"strings"
 
 	"github.com/charmbracelet/glamour"
 	"github.com/charmbracelet/glamour/styles"
@@ -39,6 +40,24 @@ func (b ToolAfterEventHandler) Handle(ent event.Event) {
 		panic("ToolEvent handler is not a ToolEvent")
 	}
 
+	if strings.HasSuffix(toolEvent.command, ".md") {
+		b.markdown(toolEvent.content)
+		return
+	}
+
+	if strings.HasSuffix(toolEvent.command, ".go") {
+		b.markdown(fmt.Sprintf("```go\n%s\n```", toolEvent.content))
+		return
+	}
+
+	if strings.HasSuffix(toolEvent.command, ".json") {
+		b.markdown(fmt.Sprintf("```json\n%s\n```", toolEvent.content))
+		return
+	}
+	b.markdown(fmt.Sprintf("```txt\n%s\n```", toolEvent.content))
+}
+
+func (b ToolAfterEventHandler) markdown(ctx string) {
 	renderer, err := glamour.NewTermRenderer(
 		glamour.WithStyles(styles.ASCIIStyleConfig),
 		glamour.WithWordWrap(-1),
@@ -48,7 +67,7 @@ func (b ToolAfterEventHandler) Handle(ent event.Event) {
 		panic(err)
 	}
 
-	cmd, err := renderer.Render(toolEvent.content)
+	cmd, err := renderer.Render(ctx)
 	if err != nil {
 		panic(err)
 	}
